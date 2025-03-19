ca9032514716884472ee34770eca05730e04b7f5
Ashish Bhosale <bhosaleashish311@gmail.com>
2025-03-19 18:00:04 +0530

    added server backend

--- src/Redux/API/axiosInstance.js
+++ src/Redux/API/axiosInstance.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: 'http://192.168.1.45:8000/api', 
-    //baseURL: 'http://localhost:5000', // JSON Server URL
+    baseURL: 'https://medical-backend-rxuk.onrender.com/api', 
+    // baseURL: 'http://192.168.1.45:8000/api', //priyanshu's server/gayatri
+    // baseURL: 'http://localhost:5000', // JSON Server URL
     headers: {
         'Content-Type': 'application/json',
     },
